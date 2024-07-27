cd6072f70fb8641eace9fe59ae10600c506164b2
caw8592 <caidenwilliams45@gmail.com>
2024-07-27 13:57:02 -0500

    finished projects and created files for each tab

---
+++ projects.js
@@ -0,0 +1,23 @@
+function tcp_button() {
+    open("https://github.com/caw8592/TCP-IP-Hackathon")
+}
+
+function calc_button() {
+    open("https://github.com/caw8592/RITGPACalculator")
+}
+
+function pltfrm_button() {
+    open("https://github.com/caw8592/SlimePlatformer")
+}
+
+function hbhacks_button() {
+    open("https://github.com/caw8592/HumanBenchmarkHacks")
+}
+
+function clicker_button() {
+    open("https://github.com/caw8592/AutoClickerv1.0")
+}
+
+function database_button() {
+    open("https://github.com/sfd2555/PDM-Project")
+}
