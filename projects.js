bd39f55f5126ea9b6fef5753b8f047dbd4c0a0c8
caw8592 <caidenwilliams45@gmail.com>
2025-05-11 16:31:30 -0400

    projects page updated

--- projects.js
+++ projects.js
@@ -1,25 +1,41 @@
-function tcp_button() {
-    open("https://github.com/caw8592/TCP-IP-Hackathon")
-}
-
-function calc_button() {
-    open("https://github.com/caw8592/RITGPACalculator")
-}
-
-function pltfrm_button() {
-    open("https://github.com/caw8592/SlimePlatformer")
-}
-
-function hbhacks_button() {
-    open("https://github.com/caw8592/HumanBenchmarkHacks")
-}
-
-function clicker_button() {
-    open("https://github.com/caw8592/AutoClickerv1.0")
-}
-
-function database_button() {
-    open("https://github.com/sfd2555/PDM-Project")
-}
-
 repos = "https://api.github.com/users/caw8592/repos"
+
+fetch(repos)
+    .then(response => response.json())
+    .then(repos => {
+        const list = document.getElementById('repo-list');
+        repos.forEach(repo => {
+            const project = document.createElement('li');
+
+            const name = document.createElement('h2')
+            name.textContent = repo.name;
+            if(repo.name == "caw8592.github.io")
+                name.textContent = "Portfolio Website";
+            project.appendChild(name);
+
+            const language = document.createElement('p');
+            language.textContent += "Language: " + repo.language;
+            project.appendChild(language);
+
+            const description = document.createElement('p');
+            description.textContent = "Description: ";
+            if(repo.description != null)
+                description.textContent += repo.description;
+            else
+                description.textContent += "This project is still being worked on, come back later to see how it turns out!";
+            project.appendChild(description);
+
+            const date = document.createElement('p');
+            date.textContent = "Last Updated: " + repo.pushed_at.slice(0, 10);
+            project.appendChild(date);
+
+            const url = document.createElement('a');
+            url.href = repo.svn_url;
+            url.textContent = "Look at the github!";
+            url.target = "_blank";
+            project.appendChild(url);
+
+            list.appendChild(project);
+        });
+    })
+    .catch(error => console.error('Error fetching repos:', error));
